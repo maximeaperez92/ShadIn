0bbd3e7c1a84f08706274bf6c751d9f416360145
‘maximeaperez92’ <maxime.aperez92@gmail.com>
2020-07-19 21:04:13 +0200

    update

--- Shader/src/Application.cpp
+++ Shader/src/Application.cpp
@@ -1,4 +1,8 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
+
 
 int main(void)
 {
@@ -19,17 +23,34 @@ int main(void)
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
+
+	if (glewInit() != GLEW_OK)
+		std::cout << "Error" << std::endl;
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
+	float positions[6] = {
+		-0.5f, -0.5f,
+		 0.0f,  0.5f,
+		 0.5f, -0.5f
+	};
+
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-1.0f, -1.0f);
-		glVertex2f(0.0f, 1.0f);
-		glVertex2f(1.0f, -1.0f);
-		glEnd();
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
